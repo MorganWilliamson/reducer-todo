823a0406f3c42575562493aaddfe87ea8e404be5
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-02 17:19:58 -0600

    Added component file, working on reducer function.

---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,22 @@
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+];
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        case "ADD_TASK":
+            return[
+                ...state, 
+                {
+                    item: action.item,
+                    completed: false,
+                    id: Date.now(),
+                },
+            ];
+            
+    }
+}
