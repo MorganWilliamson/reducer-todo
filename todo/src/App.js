a7874279196ea80bd5277c4b81361e692503eb6d
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-02 17:59:54 -0600

    Tasks rendering, functions work, text not carried over.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ToDo from "./components/ToDo";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>beep-boop</p>
+      <ToDo />
     </div>
   );
 }
--- todo/src/components/ToDo.js
+++ todo/src/components/ToDo.js
@@ -26,7 +26,21 @@ const ToDo = () => {
                     value={newTodo}
                     onChange={handleChanges}
                 />
+                <button className="add" onClick={() => dispatch({ type: "ADD_TASK", payload: newTodo })}>Create a Task</button>
             </form>
+            <ul>
+                {state.map(todo => (
+                    <li className={todo.completed ? "completed" : ""}
+                        key={todo.id}
+                        onClick={() => dispatch({ type: "TOGGLE_COMPLETE", id: todo.id })}
+                    >
+                        {todo.item}
+                    </li>
+                ))}
+            </ul>
+            <button className="remove" onClick={() => dispatch({ type: "REMOVE_TASK", payload: state })}>Clear Completed Tasks</button>
         </>
     )
 }
+
+export default ToDo;
